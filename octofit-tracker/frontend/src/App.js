0f368cd74da643ec60feabaaa6ca51a4404c492b
sanjoy-a-dutta <sanjoy.a.dutta@accenture.com>
2026-03-09 07:09:32 +0000

    modified changes

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation';
-import Dashboard from './components/Dashboard';
+
+import logo from './logo.svg';
+import './App.css';
+
 
 function App() {
   return (
-    <div className="App bg-light min-vh-100">
-      <Navigation />
-      <div className="container py-4">
-        <Dashboard />
-      </div>
+    <div className="App">
+      <nav className="navbar">
+        <img src={logo} className="octofit-logo-small navbar-logo" alt="OctoFit Logo" />
+        <span className="navbar-title">OctoFit Tracker</span>
+      </nav>
+      <header className="App-header">
+        <h1>Welcome to OctoFit Tracker</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
